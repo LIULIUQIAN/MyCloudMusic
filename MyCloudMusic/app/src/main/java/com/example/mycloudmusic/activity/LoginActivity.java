a5c6a2a3151262a9587575c69c4c6fb1778340d2
3284904004@qq.com <FFzf2016>
2020-01-18 22:17:57 +0800

    登录页面绑定事件

--- MyCloudMusic/app/src/main/java/com/example/mycloudmusic/activity/LoginActivity.java
+++ MyCloudMusic/app/src/main/java/com/example/mycloudmusic/activity/LoginActivity.java
@@ -3,14 +3,38 @@ package com.example.mycloudmusic.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.mycloudmusic.R;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
 public class LoginActivity extends BaseTitleActivity {
+
+    @BindView(R.id.et_username)
+    EditText et_username;
+
+    @BindView(R.id.et_password)
+    EditText et_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @OnClick(R.id.bt_login)
+    public void onLoginClick(){
+
+        System.out.println("=============="+et_username.getText().toString().trim());
+
+    }
+
+    @OnClick(R.id.bt_forget_password)
+    public void onForgetPasswordClick(){
+
+        System.out.println("=============="+et_password.getText().toString().trim());
+
     }
 }
