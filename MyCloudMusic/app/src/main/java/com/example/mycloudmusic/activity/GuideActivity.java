ec0ef06ed65826815ca58143281a4f2b78c99577
3284904004@qq.com <FFzf2016>
2020-01-18 17:42:36 +0800

    引导页指示器

--- MyCloudMusic/app/src/main/java/com/example/mycloudmusic/activity/GuideActivity.java
+++ MyCloudMusic/app/src/main/java/com/example/mycloudmusic/activity/GuideActivity.java
@@ -14,12 +14,15 @@ import com.example.mycloudmusic.fragment.GuideFragment;
 import java.util.ArrayList;
 import java.util.List;
 
+import me.relex.circleindicator.CircleIndicator;
+
 public class GuideActivity extends BaseCommonActivity implements View.OnClickListener {
 
     private Button bt_login_or_register;
     private Button bt_enter;
     private GuideAdapter adapter;
     private ViewPager vp;
+    private CircleIndicator indicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,7 @@ public class GuideActivity extends BaseCommonActivity implements View.OnClickLis
         //隐藏状态栏
         hideStatusBar();
         vp = findViewById(R.id.vp);
+        indicator = findViewById(R.id.indicator);
         bt_login_or_register = findViewById(R.id.bt_login_or_register);
         bt_enter = findViewById(R.id.bt_enter);
 
@@ -44,6 +48,9 @@ public class GuideActivity extends BaseCommonActivity implements View.OnClickLis
         super.initDatum();
         adapter = new GuideAdapter(getSupportFragmentManager());
         vp.setAdapter(adapter);
+
+        indicator.setViewPager(vp);
+        adapter.registerDataSetObserver(indicator.getDataSetObserver());
 
         List<Integer> datas = new ArrayList<>();
         datas.add(R.drawable.guide1);
