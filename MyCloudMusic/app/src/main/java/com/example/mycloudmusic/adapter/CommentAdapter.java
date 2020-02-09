c71c96a5a102896d9aa34d86cf2a7d104d0d6bb9
3284904004@qq.com <FFzf2016>
2020-02-09 12:37:39 +0800

    时间格式化

--- MyCloudMusic/app/src/main/java/com/example/mycloudmusic/adapter/CommentAdapter.java
+++ MyCloudMusic/app/src/main/java/com/example/mycloudmusic/adapter/CommentAdapter.java
@@ -1,19 +1,26 @@
 package com.example.mycloudmusic.adapter;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mycloudmusic.R;
 import com.example.mycloudmusic.domain.Comment;
+import com.example.mycloudmusic.util.ImageUtil;
+import com.example.mycloudmusic.util.TimeUtil;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.Inflater;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentViewHolder> {
 
@@ -29,12 +36,12 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
     @NonNull
     @Override
     public CommentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new CommentViewHolder(inflater.inflate(R.layout.item_comment,parent,false));
+        return new CommentViewHolder(context, inflater.inflate(R.layout.item_comment, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull CommentViewHolder holder, int position) {
-
+        holder.bindData(datum.get(position));
     }
 
     @Override
@@ -50,8 +57,35 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
 
     public class CommentViewHolder extends RecyclerView.ViewHolder {
 
-        public CommentViewHolder(@NonNull View itemView) {
+        private final CircleImageView iv_avatar;
+        private final TextView tv_nickname;
+        private final TextView tv_time;
+        private final TextView tv_like_count;
+        private final ImageView iv_like;
+        private final TextView tv_content;
+        private final Context context;
+
+        public CommentViewHolder(Context context, @NonNull View itemView) {
             super(itemView);
+            this.context = context;
+            iv_avatar = itemView.findViewById(R.id.iv_avatar);
+            tv_nickname = itemView.findViewById(R.id.tv_nickname);
+            tv_time = itemView.findViewById(R.id.tv_time);
+            tv_like_count = itemView.findViewById(R.id.tv_like_count);
+            iv_like = itemView.findViewById(R.id.iv_like);
+            tv_content = itemView.findViewById(R.id.tv_content);
+        }
+
+        public void bindData(Comment comment) {
+
+            ImageUtil.showAvatar((Activity) context, iv_avatar, comment.getUser().getAvatar());
+            tv_nickname.setText(comment.getUser().getNickname());
+            tv_like_count.setText(String.valueOf(comment.getLikes_count()));
+            iv_like.setImageResource(comment.isLiked() ? R.drawable.ic_comment_liked : R.drawable.ic_comment_like);
+            tv_content.setText(comment.getContent());
+
+            //时间
+            tv_time.setText(TimeUtil.commonFormat(comment.getCreated_at()));
         }
     }
 }
